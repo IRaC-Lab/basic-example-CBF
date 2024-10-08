9c1057f2f9cd893e02942b9d1aafababc1bcd1f4
IRaCLab <60131899+IRaCLab@users.noreply.github.com>
2024-10-08 15:56:21 +0900

    CVXPY사용한 kinematics control 예제 코드 완료

---
+++ src/KinematicControl_CBF_cvxpy.py
@@ -0,0 +1,63 @@
+# Import packages.
+import cvxpy as cp
+import numpy as np
+
+
+def manipulator_cbf(q, p, J, u_des, gamma):
+    # Parameters
+    R = 0.15
+    R_w = 0.05
+    p_obs = np.array([0, 0.45, 0.2])
+
+    q = np.array(q)
+    p = np.array(p)
+    J = np.array(J)
+    u_des = np.array(u_des)
+
+    # Functions
+    fx = np.zeros((6,1))
+    gx = np.eye(6)
+    
+    # Derivative of Barrier function
+    dhdx = 2*(p - p_obs).transpose() @ J[3:, :]
+    
+    # Barrier function
+    hx = np.linalg.norm(p - p_obs)**2 - (R + R_w)**2
+    
+    P = np.eye(6)
+
+    # Define and solve the CVXPY problem.
+    u = cp.Variable(6)
+    cost = cp.quad_form(u - u_des, P)
+    constraint = [(dhdx @ fx).flatten() + dhdx @ gx @ u + (hx*gamma).flatten() >=0 ]
+    prob = cp.Problem(cp.Minimize(cost), constraint)
+    prob.solve()
+
+    if prob.status == "optimal":
+         return u.value
+    else:
+         print("Status: ", prob.status)
+         return None
+
+
+def main():
+
+    # Test parameters
+    q = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    p = [0.431800000631427, -0.129539999234082, 1.05565900023251]
+    J = [[0, 0, 0, 0, 0, 0], 
+         [0.0000, -1.0000,   -1.0000,         0,   -1.0000,         0],
+    [1.0000,    0.0000,    0.0000,    1.0000 ,   0.0000,    1.0000],
+    [0.1295,  -0.4854,   -0.4854,   -0.0000,   -0.0533,         0],
+    [0.4318,    0.0000,    0.0000,         0,         0,         0],
+   [-0.0000,    0.4318,    0.0000,         0,         0,         0]]
+    u_des = [1, 0, 0, 0, 0, 0]
+    gamma = 1
+
+    u_act = manipulator_cbf(q, p, J, u_des, gamma)
+
+    print(f"actual u: {u_act}")
+          
+if __name__ == "__main__":
+	main()
+
